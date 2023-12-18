1eebd718241a940913d2508427862fab23abc4a8
Jesse Granier <jesse.granier@lapiscine.pro>
2023-12-18 14:44:03 +0100

    J'sais plus le combien... On commente le login et le token

--- coworking-front-dec2023/src/page/admin/DashboardPage.jsx
+++ coworking-front-dec2023/src/page/admin/DashboardPage.jsx
@@ -1,12 +1,28 @@
+import { useNavigate } from "react-router-dom";
 import HeaderAdmin from "../../component/admin/HeaderAdmin";
+import { useEffect } from "react";
 
 const DashboardPage = () => {
-    return(
-    <main>
-        <HeaderAdmin />
-        <h2>Je suis le Dashboard Page</h2>
-    </main>
-    )
-}
+    // Fonction permettant de rediriger l'utilisateur. Dans notre cas la redirection si réponse négative du token
+  const navigate = useNavigate();
+
+    // On fait un useEffect pour la vérification du token la première fois
+  useEffect(() => {
+    // Si présent on le met dans local storage
+    const token = localStorage.getItem("jwt");
+    // Si absent on redirige l'utilisater vers la page de login afin de sécuriser la page admin
+    if (!token) {
+      navigate("/login");
+    }
+  });
+
+  return (
+    <>
+      <HeaderAdmin />
+
+      <h2>Vous êtes bien connecté en tant qu'admin</h2>
+    </>
+  );
+};
 
 export default DashboardPage;
--- coworking-front-dec2023/src/page/guest/LoginPage.jsx
+++ coworking-front-dec2023/src/page/guest/LoginPage.jsx
@@ -1,36 +1,38 @@
 const LoginPage = () => {
-    // Async sur le event du fait de l'interrogation de l'API. Pas besoin de l'auto-appeler car elle se déclenche sur le Submit.
+        // Async sur le event du fait de l'interrogation de l'API. Pas besoin de l'auto-appeler car elle se déclenche sur le Submit.
     const handleLogin = async (event) => {
-        //On évite le rechargement de la page
       event.preventDefault();
+  
         // Récup en variables des valeurs de username et pwd
       const username = event.target.username.value;
       const password = event.target.password.value;
   
         // Création d'un objet qui stock la réponse
-      const loginData = {
+        const loginData = {
         username,
         password,
       };
-      
+  
+        // On transforme en JSON
       const loginDataJson = JSON.stringify(loginData);
-
+  
         // On fait une requête fetch pour vérifier si le user est présent dans la base de données
-      const loginResponse = await fetch("http://localhost:3000/api/users/login", {
+      const loginResponse = await fetch("http://localhost:3005/api/users/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
         },
         body: loginDataJson,
       });
-      // On convertit la réponse et on récupère le token, que l'on affiche en console.log
+  
+        // On convertit la réponse et on récupère le token, que l'on affiche en console.log
       const loginResponseData = await loginResponse.json();
       const token = loginResponseData.data;
   
-      console.log(token);
-  
-      // si l'api valide => jwt dans la réponse
-      // sinon => erreur dans la réponse
+      // Si le token est présent il est stocké dans local storage
+      if (token) {
+        localStorage.setItem("jwt", token);
+      }
     };
   
     return (
