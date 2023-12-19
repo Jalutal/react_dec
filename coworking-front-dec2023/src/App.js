3aaa76a5e428ee79ed252b75845e3c86c6d52836
Jesse Granier <jesse.granier@lapiscine.pro>
2023-12-19 14:55:47 +0100

    Le update

--- coworking-front-dec2023/src/App.js
+++ coworking-front-dec2023/src/App.js
@@ -22,7 +22,7 @@ function App() {
         <Route path="/admin/" element={<DashboardPage />} />
         <Route path="/admin/coworkings" element={<AdminCoworkingsPage />} />
         <Route path="/admin/coworkings/create" element={<AdminCoworkingCreate />} />
-        <Route path="/admin/coworkings/update" element={<AdminCoworkingUpdate />} />
+        <Route path="/admin/coworkings/update:id" element={<AdminCoworkingUpdate />} />
       </Routes>
     </BrowserRouter>
   );
